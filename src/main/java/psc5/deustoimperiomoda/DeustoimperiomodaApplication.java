f60192e6eb22738905a5ad0cc0d769405834c31c
IgnacioHDeusto <Ignacio.h.l@opendeusto.es>
2024-04-03 01:19:05 +0200

    version 1.0

---
+++ src/main/java/psc5/deustoimperiomoda/DeustoimperiomodaApplication.java
@@ -0,0 +1,13 @@
+package psc5.deustoimperiomoda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeustoimperiomodaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeustoimperiomodaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/psc5/deustoimperiomoda/DeustoimperiomodaApplicationTests.java
@@ -0,0 +1,13 @@
+package psc5.deustoimperiomoda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeustoimperiomodaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
